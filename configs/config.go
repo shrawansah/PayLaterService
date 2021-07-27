3cd5143da2656ccd555c20bc0ba0d8da8cbbd1c4
shrawansah <shrawan.sah@undostres.com.mx>
2021-07-27 16:55:58 +0530

    - generic configs

--- configs/config.go
+++ configs/config.go
@@ -17,9 +17,9 @@ var Configs = configs {
 	Environment: "development",
 	
 	DBHost: "127.0.0.1",
-	DBPassword: "iuseNOTE4@OHAAHAI",
-	DBUser: "root",
-	DBSchema: "simpl_paylater",
+	DBPassword: "YOUR_PASSWORD",
+	DBUser: "YOUR_USER",
+	DBSchema: "YOUR_SCHEMA",
 	DBPort: "3306",
 	DBDriver: "mysql",
 
